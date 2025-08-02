b636ccd51a0232608a5f0668e4b85cc44d513f43
Nourhannader <nourhannader425@gmail.com>
2025-08-02 20:03:05 +0200

    update schedule

--- src/Pages/schedule/schedule.component.ts
+++ src/Pages/schedule/schedule.component.ts
@@ -134,10 +134,10 @@ renderAppointments() {
   if (this.form.valid) {
     const selected = this.form.value;
     const localDate: Date = this.convertToDate(selected.day, selected.hour);
-    const appointmentDate1: string = localDate.toLocaleString('sv-SE').replace('T', ' '); 
+    const appointmentDate1: string = this.formatToSqlDate(localDate)
    
     const appointment={
-      appointmentDate :localDate,
+      appointmentDate :appointmentDate1,
       advertisementId: this.selectedAdId
     }
     console.log(appointment);
@@ -201,7 +201,7 @@ convertToDate(fullDay: string, hour: string): Date {
 
   let hours = 0, minutes = 0;
   const hourLower = hour.toLowerCase().trim();
-  const [timePart, period] = hourLower.split(/\s+/); // ← safe split
+  const [timePart, period] = hourLower.split(/\s+/);
   [hours, minutes] = timePart.split(':').map(Number);
 
   if (period === 'pm' && hours < 12) hours += 12;
@@ -211,4 +211,18 @@ convertToDate(fullDay: string, hour: string): Date {
 
   return date;
 }
+  
+
+formatToSqlDate(date: Date): string {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const hours = date.getHours().toString().padStart(2, '0');
+  const minutes = date.getMinutes().toString().padStart(2, '0');
+  const seconds = date.getSeconds().toString().padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}.0000000`;
+}
+
+
 }
